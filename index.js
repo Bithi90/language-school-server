a56736dcfe2345d868ad565d2170e3b67fc1d4fe
Bithi90 <bka3670@gmail.com>
2023-10-25 11:53:53 +0600

    second update

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware 
 const corsOptions = {
     origin: '*',
     credentials: true,
     optionSuccessStatus: 200,
-}
+} 
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.avf7i9k.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
